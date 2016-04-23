bd21f2341b66e4cc71bef62e37edff85331f9593
Enrikerf <enrike9066@gmail.com>
2016-04-23 14:40:02 +0200

    Conseguido oauth1, investigando oauth2

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 var express         = require('express');
 var path            = require('path');
-var favicon         = require('serve-favicon');
 var logger          = require('morgan');
 var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var app             = express();
+var stormpath       = require('express-stormpath');
 
+var config = require('./config/config.json');
 //configuración para ejs
 app.set('views', path.join(__dirname, 'views'));
-app.engine("html", require("ejs").renderFile);
-app.set('view engine', 'html');
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 //configuramos methodOverride
 app.use(methodOverride(function(req, res){
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -25,13 +24,26 @@ app.use(methodOverride(function(req, res){
         return method
     }
 }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(stormpath.init(app,{
+    client: {
+        apiKey:{
+            id: config.stormpath.client.apiKey.id,
+            secret: config.stormpath.client.apiKey.secret
+        }
+    },
+    application:{
+        href : config.stormpath.application.href
+    }
+}));
 
 
 
 routes = require('./routes/users')(app);
 
+app.get('/test',stormpath.apiAuthenticationRequired,  function(req,res){
+    res.json({test:"exito"});
+});
 
 
 // catch 404 and forward to error handler
